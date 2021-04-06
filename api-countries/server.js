b3236358148899d3cb707e3dc13d9d0074efcbde
unknown <jadisnou@gmail.com>
2021-04-06 17:02:38 +0200

    fin exo

--- api-countries/server.js
+++ api-countries/server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
+var cors = require('cors')
+app.use(cors())
 const port = 8000;
 
-// app.get('/', (req, res) => {
-// res.send('Countries API');
-// });
+app.get('/', (req, res) => {
+    res.send('Countries API');
+});
 
 
 app.get('/countries/', (req, res) => {
-    
+
     var list = ['Tunisie', 'Maroc', 'Algerie', 'Senegal']
 
-    
 
-   
+
+
     res.send(list);
 
 });
